7dd91f6e720d891b97686481a57f59527970ea44
Griffindor301 <griffhoover@outlook.com>
2023-03-25 22:43:31 -0400

    Updated Docker Image to support Carbon Modding Framework

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,45 @@
 cd /home/container
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
 # Update Rust Server
-./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 258550 +quit
+./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 +quit
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo -e "\033[1;33mcustomer@sparkedhost:~\$\033[0m ${MODIFIED_STARTUP}"
 
-# OxideMod has been replaced with uMod
-if [ -f OXIDE_FLAG ] || [ "${OXIDE}" = 1 ] || [ "${UMOD}" = 1 ]; then
+if [ -f CARBON_FLAG ] || [ "${FRAMEWORK}" == "carbon" ]; then
+    # Carbon: https://github.com/CarbonCommunity/Carbon.Core
+    echo "Updating Carbon Modding Framework..."
+    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
+    echo "Done updating Carbon Modding Framework!"
+
+    # backward compatibility
+    export CARBON=1
+
+    export DOORSTOP_ENABLED=1
+    export DOORSTOP_TARGET_ASSEMBLY="$(pwd)/carbon/managed/Carbon.Preloader.dll"
+    MODIFIED_STARTUP="LD_PRELOAD=$(pwd)/libdoorstop.so ${MODIFIED_STARTUP}"
+
+elif [ -f OXIDE_FLAG ] || [ "${FRAMEWORK}" == "oxide" ]; then
+    # Oxide: https://github.com/OxideMod/Oxide.Rust
     echo "Updating uMod..."
-    curl -sSL "https://umod.org/games/rust/download/develop" > umod.zip
+    curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
     unzip -o -q umod.zip
     rm umod.zip
     echo "Done updating uMod!"
+
+    # backward compatibility
+    export OXIDE=1
+    export UMOD=1
+
+# else Vanilla, do nothing
 fi
 
 # Fix for Rust not starting
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+export LD_LIBRARY_PATH=$(pwd)/RustDedicated_Data/Plugins/x86_64:$(pwd)
 
 # Run the Server
 node /wrapper.js "${MODIFIED_STARTUP}"
